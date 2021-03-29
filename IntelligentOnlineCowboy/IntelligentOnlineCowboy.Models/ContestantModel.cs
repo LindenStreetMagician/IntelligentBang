742de22a7fbcdc35fff9fe90b93f7386cca2d83a
Kristóf Seregley <kristof.seregley@gmail.com>
2021-03-29 22:44:20 +0200

    Fix ContestantModel

--- IntelligentOnlineCowboy/IntelligentOnlineCowboy.Models/ContestantModel.cs
+++ IntelligentOnlineCowboy/IntelligentOnlineCowboy.Models/ContestantModel.cs
@@ -4,7 +4,7 @@ namespace IntelligentOnlineCowboy.Models
 {
     public class ContestantModel
     {
-        public Guid Id { get; } = new Guid();
+        public Guid Id { get; } = Guid.NewGuid();
         public string Name { get; set; }
     }
 }
