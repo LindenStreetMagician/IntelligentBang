03306cb787b781533998dd84fa1061c7b60164fb
Kristóf Seregley <kristof.seregley@gmail.com>
2021-03-29 19:32:28 +0200

    Add contestants and next round button

--- IntelligentOnlineCowboy/IntelligentOnlineCowboy/MainWindow.Designer.cs
+++ IntelligentOnlineCowboy/IntelligentOnlineCowboy/MainWindow.Designer.cs
@@ -35,6 +35,9 @@ namespace IntelligentOnlineCowboy
             this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contestantTextBox1 = new System.Windows.Forms.TextBox();
+            this.contestantTextBox2 = new System.Windows.Forms.TextBox();
+            this.NextRoundButton = new System.Windows.Forms.Button();
             this.mainMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,7 +47,7 @@ namespace IntelligentOnlineCowboy
             this.menuToolStripMenuItem});
             this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
             this.mainMenuStrip.Name = "mainMenuStrip";
-            this.mainMenuStrip.Size = new System.Drawing.Size(800, 24);
+            this.mainMenuStrip.Size = new System.Drawing.Size(1145, 24);
             this.mainMenuStrip.TabIndex = 0;
             this.mainMenuStrip.Text = "mainMenuStrip";
             this.mainMenuStrip.TextDirection = System.Windows.Forms.ToolStripTextDirection.Vertical90;
@@ -81,16 +84,54 @@ namespace IntelligentOnlineCowboy
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             this.exitToolStripMenuItem.Text = "E&xit";
             // 
+            // contestantTextBox1
+            // 
+            this.contestantTextBox1.Font = new System.Drawing.Font("Segoe UI", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.contestantTextBox1.Location = new System.Drawing.Point(320, 57);
+            this.contestantTextBox1.Name = "contestantTextBox1";
+            this.contestantTextBox1.ReadOnly = true;
+            this.contestantTextBox1.Size = new System.Drawing.Size(813, 114);
+            this.contestantTextBox1.TabIndex = 3;
+            this.contestantTextBox1.Text = "TESZT BLASZT 1";
+            // 
+            // contestantTextBox2
+            // 
+            this.contestantTextBox2.Font = new System.Drawing.Font("Segoe UI", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.contestantTextBox2.Location = new System.Drawing.Point(320, 390);
+            this.contestantTextBox2.Name = "contestantTextBox2";
+            this.contestantTextBox2.ReadOnly = true;
+            this.contestantTextBox2.Size = new System.Drawing.Size(813, 114);
+            this.contestantTextBox2.TabIndex = 3;
+            this.contestantTextBox2.Text = "TESZT BLASZT 2";
+            // 
+            // NextRoundButton
+            // 
+            this.NextRoundButton.BackColor = System.Drawing.Color.Black;
+            this.NextRoundButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.NextRoundButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.NextRoundButton.FlatAppearance.BorderSize = 0;
+            this.NextRoundButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.NextRoundButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.NextRoundButton.ForeColor = System.Drawing.Color.Transparent;
+            this.NextRoundButton.Image = ((System.Drawing.Image)(resources.GetObject("NextRoundButton.Image")));
+            this.NextRoundButton.Location = new System.Drawing.Point(12, 220);
+            this.NextRoundButton.Name = "NextRoundButton";
+            this.NextRoundButton.Size = new System.Drawing.Size(252, 145);
+            this.NextRoundButton.TabIndex = 4;
+            this.NextRoundButton.UseVisualStyleBackColor = false;
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1145, 564);
+            this.Controls.Add(this.NextRoundButton);
+            this.Controls.Add(this.contestantTextBox2);
+            this.Controls.Add(this.contestantTextBox1);
             this.Controls.Add(this.mainMenuStrip);
             this.MainMenuStrip = this.mainMenuStrip;
             this.Name = "MainWindow";
             this.Text = "Intelligent Online Cowboy";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.mainMenuStrip.ResumeLayout(false);
             this.mainMenuStrip.PerformLayout();
             this.ResumeLayout(false);
@@ -105,6 +146,9 @@ namespace IntelligentOnlineCowboy
         private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox contestantTextBox1;
+        private System.Windows.Forms.TextBox contestantTextBox2;
+        private System.Windows.Forms.Button NextRoundButton;
     }
 }
 
