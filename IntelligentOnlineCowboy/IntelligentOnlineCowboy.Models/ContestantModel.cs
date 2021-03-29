7831e58cd8ec9d25be09cdc600651b9c0eacc4ff
Kristóf Seregley <kristof.seregley@gmail.com>
2021-03-29 21:50:52 +0200

    Add ContestantModel

---
+++ IntelligentOnlineCowboy/IntelligentOnlineCowboy.Models/ContestantModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace IntelligentOnlineCowboy.Models
+{
+    public class ContestantModel
+    {
+        public Guid Id { get; } = new Guid();
+        public string Name { get; set; }
+    }
+}
