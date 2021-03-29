6947b3d70c4978f06573cfd7eb5b5f34b193f3b0
Kristóf Seregley <kristof.seregley@gmail.com>
2021-03-29 22:02:24 +0200

    Add Names text file

--- IntelligentOnlineCowboy/IntelligentOnlineCowboy/Properties/Resources.Designer.cs
+++ IntelligentOnlineCowboy/IntelligentOnlineCowboy/Properties/Resources.Designer.cs
@@ -61,6 +61,15 @@ namespace IntelligentOnlineCowboy.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to .
+        /// </summary>
+        public static string Names {
+            get {
+                return ResourceManager.GetString("Names", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         public static System.Drawing.Bitmap pistol {
